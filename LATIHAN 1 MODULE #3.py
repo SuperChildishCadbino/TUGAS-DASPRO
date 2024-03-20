e52cf67cfd4ab66569abbf19dbf50e5b533fc565
M. IRSYAD S.Y <163075282+SuperChildishCadbino@users.noreply.github.com>
2024-03-20 22:00:57 +0900

    Add files via upload

---
+++ LATIHAN 1 MODULE #3.py
@@ -0,0 +1,14 @@
+#Latihan - 1
+
+# x = 10
+# y = 5
+
+#bagimana caranya menghasilkan nilai true menggunakan operator sama
+#dengan (addtion operator)
+
+x = 10
+y = 5
+
+hasil = x <= y
+
+print(x, ">" ,y,"bernilai = ",hasil)
