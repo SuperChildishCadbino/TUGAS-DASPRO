75db19a8124ab3fb360bcacec0d82183be6c06ef
M. IRSYAD S.Y <163075282+SuperChildishCadbino@users.noreply.github.com>
2024-03-20 22:27:56 +0900

    MODULE #4 - CONTROL FLOW

--- LATIHAN  1 MODULE #4.py
+++ LATIHAN  1 MODULE #4.py
@@ -1,17 +1,10 @@
-print("\n=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#")
-print("=#=#=#=# PROGRAM DISCOUNT #=#=#=#=")
-print("=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#\n")
+hitung_diskon(total_belanja):
+    if total_belanja >= 250000:
+        diskon = total_belanja * 50 / 100
+        print("Anda mendapatkan diskon sebesar Rp.", diskon)
+    else:
+        print("Total belanjaan Anda adalah Rp.", total_belanja)
+        print("Maaf, Anda tidak mendapatkan diskon karena total belanjaan tidak mencapai Rp. 250.000")
 
-print("###### Anda akan mendapatkan diskon jika total belanja anda mencapai Rp. 250.000 ######\n")
 total_belanja = float(input("Masukkan total belanjaan Anda: Rp. "))
-
-if total_belanja >= 250000:
-    diskon = total_belanja * 50 / 100
-    print("Anda mendapatkan diskon sebesar : Rp.", diskon)
-else:
-    print("Total belanjaan Anda adalah : Rp.", total_belanja)
-    print("Maaf, Anda tidak mendapatkan diskon karena total belanjaan tidak mencapai Rp. 250.000")
-
-print("\nTerima Kasih telah menggunakan program saya\n")
-print("          NAMA : M.IRSYAD S.Y              ")
-print("          NPM  : 07352311150                \n")
+hitung_diskon(total_belanja)
